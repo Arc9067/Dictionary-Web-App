94b70ca9cc2a962a60bff9b80b9ea77a1420cc00
Mikail Rokib <atandarokib@gmail.com>
2023-03-03 01:20:00 +0100

    initial commit

---
+++ assets/script/script.js
@@ -0,0 +1,109 @@
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleIcon = document.querySelector(".togglei");
+const arrowDown = document.querySelector(".currentli");
+const arrowIcon = document.querySelector(".arrow");
+const selectFont = document.querySelector(".fontt-ul");
+const addSanSerif = document.querySelector(".san-serif-li");
+const addSerif = document.querySelector(".serif-li");
+const addmono = document.querySelector(".mono-li");
+const currentli = document.querySelector(".currentli");
+const typeDisplay = document.querySelector(".type-display");
+const fieldSet = document.querySelector(".search-field");
+const searchInput = document.querySelector(".search-input");
+const searchIcon = document.querySelector(".search-icon");
+const err = document.querySelector(".error");
+const playSound = document.querySelector(".play-sound");
+const pronounce = document.querySelector(".dictionary-pronounce");
+const wordType = document.querySelector(".wtype");
+
+toggleBtn.addEventListener("click", async () => {
+  await document.documentElement.classList.toggle("dark-theme");
+  if (document.documentElement.classList.contains("dark-theme")) {
+    toggleBtn.innerHTML = `<i class="bi bi-toggle-on togglei"></i
+><i class="bi bi-moon"></i>`;
+  } else {
+    toggleBtn.innerHTML = `              <i class="bi bi-toggle-off togglei"></i
+              ><img src="./assets/images/icon-moon.svg" alt="" />
+`;
+  }
+});
+
+arrowDown.addEventListener("click", () => {
+  arrowIcon.classList.toggle("arrow-rotate");
+  selectFont.classList.toggle("dflex");
+});
+
+addSanSerif.addEventListener("click", () => {
+  if (
+    document.body.classList.contains("monospace") ||
+    document.body.classList.contains("serif")
+  ) {
+    document.body.classList.add("san-serif");
+    typeDisplay.textContent = "san serif";
+    document.body.classList.remove("monospace");
+    document.body.classList.remove("serif");
+    arrowIcon.classList.toggle("arrow-rotate");
+    selectFont.classList.toggle("dflex");
+  }
+});
+
+addSerif.addEventListener("click", () => {
+  if (
+    document.body.classList.contains("monospace") ||
+    document.body.classList.contains("san-serif")
+  ) {
+    document.body.classList.add("serif");
+    typeDisplay.textContent = "serif";
+    document.body.classList.remove("monospace");
+    document.body.classList.remove("san-serif");
+    arrowIcon.classList.toggle("arrow-rotate");
+    selectFont.classList.toggle("dflex");
+  }
+});
+
+addmono.addEventListener("click", () => {
+  if (
+    document.body.classList.contains("serif") ||
+    document.body.classList.contains("san-serif")
+  ) {
+    document.body.classList.add("monospace");
+    typeDisplay.textContent = "monospace";
+    document.body.classList.remove("serif");
+    document.body.classList.remove("san-serif");
+    arrowIcon.classList.toggle("arrow-rotate");
+    selectFont.classList.toggle("dflex");
+  }
+});
+
+searchIcon.addEventListener("click", () => {
+  if (searchInput.value === "") {
+    if (!fieldSet.classList.contains("fieldActive")) {
+      fieldSet.classList.remove("fieldActive");
+      err.style.display = "block";
+      fieldSet.classList.add("fieldErr");
+    }
+  } else {
+    err.style.display = "none";
+    fieldSet.classList.remove("fieldErr");
+  }
+});
+
+searchInput.addEventListener("keyup", () => {
+  fieldSet.classList.add("fieldActive");
+  if (searchInput.value != "") {
+    fieldSet.classList.remove("fieldErr");
+    err.style.display = "none";
+  } else {
+    fieldSet.classList.remove("fieldActive");
+    err.style.display = "block";
+    fieldSet.classList.add("fieldErr");
+    setTimeout(() => {
+      err.style.display = "none";
+      fieldSet.classList.remove("fieldErr");
+    }, 3000);
+  }
+});
+
+playSound.addEventListener("click", () => {
+  pronounce.play();
+});
